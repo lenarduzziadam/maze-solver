8546bddad81dd34e371ea918dea8dbb1e262ab08
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-16 15:25:34 -0500

    added initialization for window

---
+++ window.py
@@ -0,0 +1,35 @@
+from tkinter import Tk, BOTH, Canvas
+import tkinter as tk
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+        #creating root widget
+        self.__root = Tk()
+        #set root widget title
+        self.__root.title('Maze Solver')
+        
+        #create canvas with root keeping width and height as parameters
+        canvas = Canvas(self.__root, width=width, height=height)
+        #packs canvas to prepare for drawing
+        canvas.pack()
+        
+        #creates running variable setting it to false
+        self.running = False
+        
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update()
+        self.__root.update_idletasks()
+        
+    def wait_for_close(self):
+        #sets self.running to true then iterates through loop until running is no longer true
+        self.running = True
+        while self.running:
+            self.redraw()
+            
+    def close(self):
+        self.running = False
