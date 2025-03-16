7cb162460eaa4b397d9c3c8a53b273c99256e012
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-16 16:54:07 -0500

    added line drawer to maze solver program

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from window import *
 
 def main():
     win = Window(800, 600)
+    
+     # Create some points
+    p1 = Point(100, 100)
+    p2 = Point(200, 200)
+    p3 = Point(300, 100)
+    p4 = Point(420, 150)
+    # Create lines using those points
+    line1 = Line(p1, p2)
+    line2 = Line(p2, p3)
+    line3 = Line(p3, p1)
+    line4 = Line(p1, p4)
+    
+    # Draw the lines with different colors
+    win.draw_line(line1, "red")
+    win.draw_line(line2, "blue")
+    win.draw_line(line3, "green")
+    win.draw_line(line4, "yellow")
+    
     win.wait_for_close()
     
 if __name__ == '__main__':
