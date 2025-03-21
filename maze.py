31d7c7794d533c945f1772210d0485a4c0ecf686
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-20 20:06:26 -0500

    added more

---
+++ maze.py
@@ -0,0 +1,59 @@
+import time
+from mechanics import *
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win=None):
+        
+        self._cells = []
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        
+        self._create_cells()
+    
+    def _create_cells(self):
+        #for index in range of cols
+        for i in range(self.num_cols):
+            #creates new column list for appending every new column
+            col_cells = []
+            #iterates over rows
+            for j in range(self.num_rows):
+                #appends rows to column list
+                col_cells.append(Cell(self.win))
+            #appends column list to overall list for cells
+            self._cells.append(col_cells)
+            
+        #double iteration to draw all cells
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i, j)
+                
+    
+    def _draw_cell(self, i, j):
+        #checks if window canvas exists
+        if self.win is None:
+            return
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + j * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+        
+    def _animate(self):
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(0.025)
---
+++ mechanics.py
@@ -0,0 +1,105 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+
+class Window:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        
+        self.root = Tk()
+        self.root.title("aMAZEing Solve")
+        self.window = Canvas(self.root, bg="white", height=height, width=width)
+        self.window.pack()
+        
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        #updates window idle tasks and regular tasks
+        self.root.update()
+        self.root.update_idletasks()
+    
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.window, fill_color)
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        
+    def close(self):
+        self.running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.x1 = p1.x
+        self.x2 = p2.x
+        self.y1 = p1.y
+        self.y2 = p2.y
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+        )
+        
+class Cell:
+    def __init__(self, win):
+        
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+    
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+            
+    def draw_move(self, to_cell, undo=False):
+        #floor division to half length
+        half_length = abs(self._x2 - self._x1) // 2
+        #gets x and y center by added x and y value to half_length
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        #repeat with to_cell
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        #fill_color red if flag not set but if set instantly sets it to gray
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        #creates line for both instances
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
+
+    
