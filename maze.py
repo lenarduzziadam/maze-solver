c8d14dc37d7967f646bac9a9eeb611eef0fe7609
lenarduzziadam <lenarduzziadam@gmail.com>
2025-03-20 20:13:09 -0500

    added more

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 import time
 from mechanics import *
+from mechanics import Cell
 
 class Maze:
     def __init__(
@@ -22,6 +23,7 @@ class Maze:
         self.win = win
         
         self._create_cells()
+        self._break_entrance_and_exit()
     
     def _create_cells(self):
         #for index in range of cols
@@ -57,3 +59,9 @@ class Maze:
             return
         self.win.redraw()
         time.sleep(0.025)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self.num_cols - 1][self.num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self.num_cols - 1, self.num_rows - 1)
